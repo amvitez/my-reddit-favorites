03496c5d989827c4a64fabda1f6833b9919d36a4
Tony Vitez <amvitez@gmail.com>
2015-10-15 19:47:00 -0400

    change vars

--- web/db.php
+++ web/db.php
@@ -5,11 +5,11 @@ try {
 	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 	$server = $url["host"];
-	$username = $url["user"];
-	$password = $url["pass"];
+	$user = $url["user"];
+	$pass = $url["pass"];
 	$dbName = substr($url["path"], 1);
 
-	$db = new PDO("mysql:host=".$server.";dbname=".$dbName.";port=3306",$username,$password);
+	$db = new PDO("mysql:host=".$server.";dbname=".$dbName.";port=3306",$user,$pass);
 	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	$db->exec("SET NAMES 'utf8'");
 } catch(PDOException $e) {
